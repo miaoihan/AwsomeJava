130685c22f752bd5f08184653d059d8acde661ed
miaoihan <izhanghan@outlook.com>
2016-04-05 22:20:50 +0800

    并发

--- MultiThreading/TestThread1.java
+++ MultiThreading/TestThread1.java
@@ -4,6 +4,7 @@ package MultiThreading;
  * Created by han on 16-4-5.
  */
 public class TestThread1 {
+    public static int a = 3;
     public static void main(String[] args) {
         Thread t1 = new MyThread();
         Thread t2 = new MyThread2();
@@ -16,27 +17,29 @@ public class TestThread1 {
 class MyThread extends Thread{
     @Override
     public void run() {
-        for (int i = 0; i < 25 ; i++) {
-            System.out.print(i+" ");
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+//        for (int i = 0; i < 25 ; i++) {
+//            System.out.print(i+" ");
+//            try {
+//                Thread.sleep(5);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        System.out.println(TestThread1.a+=2);
     }
 }
 
 class MyThread2 extends Thread{
     @Override
     public void run() {
-        for (char i='a'; i<='z'; i++){
-            System.out.print(i+" ");
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+//        for (char i='a'; i<='z'; i++){
+//            System.out.print(i+" ");
+//            try {
+//                Thread.sleep(5);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+        System.out.println(TestThread1.a+=2);
     }
 }
--- MultiThreading/TestThread2.java
+++ MultiThreading/TestThread2.java
@@ -4,10 +4,25 @@ package MultiThreading;
  * Created by han on 16-4-5.
  */
 public class TestThread2 {
+    int a;
     public static void main(String[] args) {
-        Thread t1 = new MyThread();
-        t1.run();
+        MyTask1 m1 = new MyTask1();
+        Thread t1 = new Thread(m1);
+        t1.start();
+
+
     }
 
+}
+
+class MyTask1 implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i+" ");
+        }
+        //System.out.println(a+=2);
+    }
 }
 
