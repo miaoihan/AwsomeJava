83a5f20a502211cf4bab7031b3b9a2739e9dbfad
miaoihan <izhanghan@outlook.com>
2016-04-14 20:56:34 +0800

    TestDoc

---
+++ TestDoc/HelloDoc.java
@@ -0,0 +1,24 @@
+//:object/HelloDoc.java
+
+package TestDoc;
+
+import java.util.Date;
+
+/** 我的第一个doc文档
+ *  @author han
+ *  @version 4.0
+ */
+public class HelloDoc {
+    /**
+     *
+     * @param args array of string arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello now is: ");
+        System.out.println(new Date());
+    }
+} /* Output: (55% match)
+Hello, it's:
+Thu Apr 14 20:52:49 CST 2016
+
+*///:~
