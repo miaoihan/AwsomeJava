88bea6dc3ea2cb5efea79909b6df3638576bebe7
miaoihan <izhanghan@outlook.com>
2016-04-17 22:13:57 +0800

    do

---
+++ Array/TestArray.java
@@ -0,0 +1,14 @@
+package Array;
+
+/**
+ * Created by han on 16-4-16.
+ */
+public class TestArray {
+    /**  声明数组不会给数组分配空间，编译器知识创建了一个对象引用   */
+    int [] a;
+    int b[];
+    /**  new的时候必须指定数组大小   */
+    String [] s = new String[10];
+    s[0] = "el";
+
+}
