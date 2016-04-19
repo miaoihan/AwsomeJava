0ae44182d1bc13c3141ee59768988c82a5c568de
miaoihan <izhanghan@outlook.com>
2016-04-19 22:58:28 +0800

    do

---
+++ collection/GenericsAndUpcasting.java
@@ -0,0 +1,10 @@
+package collection;
+
+/**
+ * Created by han on 16-4-19.
+ */
+class Fr{}
+
+public class GenericsAndUpcasting {
+
+}
--- collection/TestMap.java
+++ collection/TestMap.java
@@ -5,13 +5,14 @@ import java.util.Map;
 import java.util.Random;
 
 /**
+ * 用HashMap统计随机分布
  * Created by han on 16-4-19.
  */
 public class TestMap {
     public static void main(String[] args) {
         Random rand = new Random();
         Map<Integer,Integer> map= new HashMap<Integer,Integer>();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000000; i++) {
             int r = rand.nextInt(20);
             Integer time = map.get(r);
             map.put(r, time == null ? 1 : time+1);
