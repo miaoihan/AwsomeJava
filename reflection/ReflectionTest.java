7cb5d94adc40099acfdf10e6838c5042147e82c1
miaoihan <izhanghan@outlook.com>
2016-04-25 19:56:44 +0800

    reflection

--- reflection/ReflectionTest.java
+++ reflection/ReflectionTest.java
@@ -1,5 +1,7 @@
 package reflection;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 import java.lang.reflect.*;
 import java.util.*;
 
@@ -28,17 +30,23 @@ public class ReflectionTest {
             System.out.print("class " + name);
             if (null != supercl && supercl != Objects.class)
                 System.out.print(" extends "+supercl.getName());
-            System.out.println("{\n");
+            System.out.println("{");
             printConstructors(cl);
+            System.out.println();
+            printMethods(cl);
+            System.out.println();
+            printFields(cl);
+            System.out.println("}");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        System.exit(0);
 
     }
 
     /**
      * print all constructors
-     * @param cl
+     * @param cl a class
      */
     public static void printConstructors(Class cl){
         Constructor[] constructors = cl.getDeclaredConstructors();
@@ -57,4 +65,44 @@ public class ReflectionTest {
             System.out.println(");");
         }
     }
+
+    /**
+     * print all Methods
+     * @param cl a class
+     */
+    public static void printMethods(Class cl){
+        Method[] methons = cl.getDeclaredMethods();
+        for (Method m : methons) {
+            Class retType = m.getReturnType();
+            String name = m.getName();
+            System.out.print("  ");
+            String modifiers = Modifier.toString(m.getModifiers());
+            if (modifiers.length()>0) System.out.print(modifiers+" ");
+            System.out.print(retType.getName()+" "+name+"(");
+            Class[] paramTypes = m.getParameterTypes();
+            for (int i = 0; i < paramTypes.length; i++) {
+                if (i > 0) System.out.print(", ");
+                System.out.print(paramTypes[i].getName());
+            }
+            System.out.println(");");
+        }
+    }
+
+    /**
+     * print all fields
+     * @param cl
+     */
+    public static void printFields(Class cl){
+        Field[] fields = cl.getFields();
+        for (Field f:fields) {
+            Class type = f.getType();
+            String name = f.getName();
+            System.out.print("  ");
+            String modifiers = Modifier.toString(f.getModifiers());
+            if (modifiers.length()>0) System.out.print(modifiers+" ");
+            System.out.println(type.getName()+" "+name+";");
+        }
+    }
+
+
 }
